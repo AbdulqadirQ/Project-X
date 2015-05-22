b34f38796372049c072d630c1885c41442e7a7ad
Kiran94 <kiran_patel94@hotmail.com>
2015-05-22 21:29:00 +0100

    fixed curly braces in some files (test)

--- quadratic_Components.java
+++ quadratic_Components.java
@@ -1,12 +1,11 @@
 // class contains a list of components used by the module class		***UPDATE COUNTER - CLASS CONTAINS (3) COMPONENTS***
-class quadratic_Components {
-	
-	
+class quadratic_Components 
+{
 	/*(1) component 1 accepts input
 	 * returns double array of values
 	 */
-	protected double[] error_Handling_Component(double a, double b, double c){
-		
+	protected double[] error_Handling_Component(double a, double b, double c)
+	{
 		double[] input = {a,b,c}; // holds the input values	
 		return input;
 	}
@@ -16,8 +15,8 @@ class quadratic_Components {
 	/*(2) component 2 takes an array of values and applies quadratic formula
 	 * returns array containing solution(s)
 	 */
-	protected double[] quadratic_Component(double[] input){
-		
+	protected double[] quadratic_Component(double[] input)
+	{
 		// apply formula and hold output in new solution array
 		double[] solution = Formula.quadraticEquation(input[0], input[1], input[2]);	
 		return solution;	
@@ -28,25 +27,10 @@ class quadratic_Components {
 	/*(3) component 3 links to graphical interface
 	 * passes solution array to GUI
 	 */
-	protected double[] output_Component(double[] solution){
-		
+	protected double[] output_Component(double[] solution)
+	{
 		// for test purposes print
 		Test.print(solution);
 		return solution;
 	}
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 }
