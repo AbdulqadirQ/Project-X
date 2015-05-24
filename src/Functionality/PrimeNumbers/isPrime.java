46730c221abe56362ca4e11c18ec4d4a73cd1947
Kiran94 <kiran_patel94@hotmail.com>
2015-05-24 15:33:21 +0100

    added comments

--- src/Functionality/PrimeNumbers/isPrime.java
+++ src/Functionality/PrimeNumbers/isPrime.java
@@ -1,11 +1,17 @@
 import java.util.ArrayList; 
 
+/*
+	Class provides application with a function to check if an integer is a prime number or not. 
+	Class uses the PrimeGen class to generate prime numbers. 
+*/
 public class isPrime
 {
+	//Function checks if an integer is a prime number. 
 	public boolean checkPrime(int toCheck)
 	{
 		//Create a new instance of the PrimeGen class. 
-		PrimeGen gen = new PrimeGen(); 
+		PrimeGen gen = new PrimeGen();
+
 		//Run the generate algorithm and pass toCheck + 1 
 		//Algorithm will find all prime numbers from 2 to toCheck+1
 		ArrayList<Integer> list = gen.generate(toCheck+1); 
